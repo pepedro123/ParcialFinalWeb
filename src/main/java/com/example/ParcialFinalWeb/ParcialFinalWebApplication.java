4294d17abe2085bc3160f1d5560c955d50339b02
Pedro Molina <pealmo98@gmail.com>
2023-12-20 08:31:03 -0500

    Spring initializer(application.properties)

---
+++ src/main/java/com/example/ParcialFinalWeb/ParcialFinalWebApplication.java
@@ -0,0 +1,13 @@
+package com.example.ParcialFinalWeb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParcialFinalWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParcialFinalWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ParcialFinalWeb/ParcialFinalWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ParcialFinalWeb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParcialFinalWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
